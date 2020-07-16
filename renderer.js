279b0fb4a6d6b3d0b5df81f97e7a551d7b8bb7ee
sehandev <sehandev@gmail.com>
2020-07-17 02:51:17 +0900

    init

---
+++ renderer.js
@@ -0,0 +1,16 @@
+// This file is required by the index.html file and will
+// be executed in the renderer process for that window.
+// No Node.js APIs are available in this process because
+// `nodeIntegration` is turned off. Use `preload.js` to
+// selectively enable features needed in the rendering
+// process.
+
+'use strict'
+
+window.$ = window.jQuery = require('jquery')
+window.Bootstrap = require('bootstrap')
+
+// 닫기 버튼 누르면 앱 종료
+document.getElementById("close-btn").addEventListener("click", e => {
+    window.close();
+});
