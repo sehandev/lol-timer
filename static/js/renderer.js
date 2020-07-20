5a09fe3ab20301b9d0dc5440d87e7af0e907c97f
sehandev <sehandev@gmail.com>
2020-07-20 16:59:16 +0900

    ADD : rune 공격 & EDIT : rune css

--- static/js/renderer.js
+++ static/js/renderer.js
@@ -11,6 +11,6 @@ window.$ = window.jQuery = require('jquery')
 window.Bootstrap = require('bootstrap')
 
 // 닫기 버튼 누르면 앱 종료
-document.getElementById("close-btn").addEventListener("click", e => {
+document.getElementById('close-btn').addEventListener('click', e => {
     window.close();
 });
