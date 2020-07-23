23e958565f5383ab8a03ab04c57fe7d87f35ac32
Sehan Kim <sehandev@gmail.com>
2020-07-23 22:08:20 +0900

    EDIT : index page for live client

---
+++ static/js/index.js
@@ -0,0 +1,45 @@
+'use strict'
+
+const { ipcRenderer } = require('electron')
+
+let player_id
+
+document.getElementById('reload-btn').onclick = () => {
+    window.location.reload()
+}
+
+function axios_live() {
+    ipcRenderer.send('request-live')
+}
+
+ipcRenderer.on('response-live', (event, data, is_ok) => {
+    if (is_ok) {
+        let player_name = data.activePlayer.summonerName
+        document.getElementById('active-player-btn').innerText = player_name
+        document.getElementById('active-player-btn').classList.remove('disabled')
+        event.sender.send('request-summoner', player_name, true)
+    } else {
+        // error
+        console.log(data)
+    }
+})
+
+ipcRenderer.on('response-summoner', (_, data, is_ok) => {
+    if (is_ok) {
+        player_id = data.id
+
+        document.getElementById('active-player-btn').onclick = () => {
+            window.location.href = 'spell.html?id=' + player_id
+        }
+    } else {
+        // error
+        console.log(data)
+    }
+})
+
+axios_live()
+
+player_id = 'ABfqliRE1NUIx1ETg1CQykZC2ef4qXs_60rXMLY4onkoEw'
+document.getElementById('active-player-btn').onclick = () => {
+    window.location.href = 'spell.html?id=' + player_id
+}
